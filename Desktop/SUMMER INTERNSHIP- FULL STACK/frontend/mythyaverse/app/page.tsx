7e083c759ab44cd9d8e08a866c2b5ebd5ec4d583
Gauri Agrawal <gauriagrawal1502@gmail.com>
2024-06-04 08:32:53 +0530

    Final Changes

---
+++ Desktop/SUMMER INTERNSHIP- FULL STACK/frontend/mythyaverse/app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to MythyaVerse</h1>
+    </main>
+  );
+}
